ed30bd3d8e01cf9213360b1b09602deeaa439d83
kaifuski <jde.ram9@gmail.com>
2020-07-03 03:14:27 -0500

    20 líneas

--- README.md
+++
@@ -1,3 +0,0 @@
-guessinggame.sh
-jue jul  2 16:08:57 CDT 2020
-20
--- guessinggame.sh
+++ guessinggame.sh
@@ -10,11 +10,15 @@ do
     if [[ $archivos -gt $directorios ]]
     then
 	echo "El número introducido es mayor que el número de archivos"
+	echo "(The introduced number is greater than the number of files)"
     elif [[ $archivos -lt $directorios ]]
     then
 	echo "El número introducido es menor que el número de archivos"
+	echo "(The introduced number is lower than the number of files)"
     else
 	echo "Ha adivinado el número de archivos"
+	echo "(You have guessed the number of files)"
 	break
     fi
 done
+echo "20th line yay!!"
